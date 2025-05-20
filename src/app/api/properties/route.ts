337d3fc8abca983c455b685ffc21e68fd575ce77
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2025-05-20 22:04:38 +0400

    20

--- src/app/api/properties/route.ts
+++ src/app/api/properties/route.ts
@@ -2,15 +2,16 @@ import { NextResponse } from 'next/server';
 import sql from 'mssql';
 
 const config: sql.config = {
-  user: process.env.MSSQL_USER || '',
-  password: process.env.MSSQL_PASSWORD || '',
-  server: process.env.MSSQL_SERVER || '',
-  database: process.env.MSSQL_DATABASE || '',
+  user: process.env.MSSQL_USER!,
+  password: process.env.MSSQL_PASSWORD!,
+  server: process.env.MSSQL_SERVER!,
+  database: process.env.MSSQL_DATABASE!,
   options: {
     encrypt: process.env.MSSQL_ENCRYPT === 'true',
     trustServerCertificate: true,
   },
 };
+
 
 export async function GET() {
   try {
